aabe355cf3032a65e7452a7aff82e3864b279c93
sajedul2022 <sajedul.idb.info@gmail.com>
2023-01-26 00:47:57 +0600

    Lead factory, Role based create multi user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,47 +3,58 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-
+use App\Models\Course;
+use App\Models\Curriculum;
+use App\Models\Lead;
 use App\Models\User;
 use Illuminate\Database\Seeder;
-use Spatie\Permission\Models\Role;
 use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
-class DatabaseSeeder extends Seeder
-{
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // \App\Models\User::factory(10)->create();
+class DatabaseSeeder extends Seeder{
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+    public function run(){
 
-        $user = new User();
-        $user->name = 'Super Admin';
-        $user->email = 'super@admin.com';
-        $user->password = bcrypt('super@admin.com');
-        $user->save();
+        $this->create_user_with_role('Super Admin', 'Super Admin', 'super-admin@lms.test');
+        $this->create_user_with_role('Communication', 'Communication Team', 'communication@lms.test');
+        $teacher = $this->create_user_with_role('Teacher', 'Teacher', 'teacher@lms.test');
 
-        $role = Role::create([
-            'name'=> 'Super Admin'
+
+        // create leads
+        Lead::factory(100)->create();
+
+        $course = Course::create([
+            'name' => 'Laravel',
+            'description' => 'Laravel is a web application framework with expressive, elegant syntax. We’ve already laid the foundation — freeing you to create without sweating the small things.',
+            'image' => 'https://laravel.com/img/logomark.min.svg',
+            'user_id' => $teacher->id
         ]);
 
-        $permission = Permission::create([
-            'name'=> 'create-admin'
+
+        Curriculum::factory(10)->create();
+    }
+
+    private function create_user_with_role($type, $name, $email){
+
+         $role = Role::create([
+                'name'=> $type
+            ]);
+
+        $user = User::create([
+            'name' => $name,
+            'email' => $email,
+            'password' => bcrypt('password')
         ]);
 
-        $role->givePermissionTo($permission);
-        $permission->assignRole($role);
+        if ($type == 'Super Admin') {
+            $permission = Permission::create([
+                'name' => 'create-admin'
+            ]);
+            $role->givePermissionTo($permission);
+        }
 
         $user->assignRole($role);
 
-
+        return $user;
     }
 }
